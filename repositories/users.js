ab44e6a5c09bcd30af81c1124fa53f926f0300b7
cyberlearner13 <arkadeepmukherjee2012@gmail.com>
2021-04-24 20:34:42 +0530

    added randomId generation

--- repositories/users.js
+++ repositories/users.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const crypto = require('crypto');
 class UsersRepository{
     constructor(filename){
         if(!filename){
@@ -18,4 +19,29 @@ class UsersRepository{
             encoding: 'utf-8'
         }));
     }
+
+    async create(attrs){
+        attrs.id = this.randomId();
+        const records = await this.getAll();
+        records.push(attrs);
+        await this.writeAll(records);
+    }
+
+    async writeAll(records){
+    // Write the updated 'records' array back to this.filename
+    await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2));
+    }
+
+    randomId(){
+        return crypto.randomBytes(4).toString('hex');
+    }
 }
+
+const test = async () => {
+    const repo = new UsersRepository('users.json');
+    await repo.create({ email: 'test@test.com', password: 'password'});
+    const users = await repo.getAll();
+    console.log(users);
+}
+
+test();
