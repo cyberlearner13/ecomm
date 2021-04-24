ee2911585338daca5143399b125cbaa2eca8b3de
cyberlearner13 <arkadeepmukherjee2012@gmail.com>
2021-04-24 18:38:04 +0530

    Added Body Parser and basic routing

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const urlencoded = require('body-parser').urlencoded;
 const app = express();
 
+app.use(urlencoded({ extended: true}));
+
 app.get('/', (req, res)=>{
-    res.send('Hello There');
+    res.send(`
+        <form method="POST">
+            <input name="hello" />
+            <button>Submit</button>
+        </form>
+    `);
 });
+
+app.post("/", (req, res)=>{
+    console.log(req.body);
+    res.send('Done!')
+})
 
 app.listen('5000', ()=> console.log('Server started'));
