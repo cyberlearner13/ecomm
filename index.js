76bf9ee18f4424b46562eb3a091724460987d3eb
cyberlearner13 <arkadeepmukherjee2012@gmail.com>
2021-05-01 15:27:03 +0530

    Added support for cookies

--- index.js
+++ index.js
@@ -1,21 +1,55 @@
 const express = require('express');
 const urlencoded = require('body-parser').urlencoded;
+const cookieSession = require('cookie-session');
+const usersRepo = require('./repositories/users');
 const app = express();
 
-app.use(urlencoded({ extended: true}));
+app.use(urlencoded({
+    extended: true
+}));
+app.use(cookieSession({
+    keys: ['sdfheufh9824an 9qweuY&4t837N_2421QINOI5486!@!#!//#endregion']
+}))
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send(`
         <form method="POST">
-            <input name="hello" />
+            <input name="email" type="email" placeholder="email" />
+            <input name="password" type="password" placeholder="password" />
+            <input name="confirmPassword" type="password" placeholder="confirmPassword" />
             <button>Submit</button>
         </form>
     `);
 });
 
-app.post("/", (req, res)=>{
-    console.log(req.body);
+app.post("/", async (req, res) => {
+    const {
+        email,
+        password,
+        confirmPassword
+    } = req.body;
+
+    const existingUser = await usersRepo.getOneBy({
+        email
+    });
+
+    if (existingUser) {
+        return res.send('Email in use');
+    }
+
+    if (password !== confirmPassword) {
+        return res.send('Passwords must match');
+    }
+
+    // Create a user in our repo to represent this person
+    const user = await usersRepo.create({
+        email,
+        password
+    });
+
+    // Store the id of that user inside of the users cookie
+    req.session.userId = user.id; // Added by cookie session
     res.send('Done!')
 })
 
-app.listen('5000', ()=> console.log('Server started'));
+app.listen('5000', () => console.log('Server started'));
--- repositories/users.js
+++ repositories/users.js
@@ -1,56 +1,57 @@
 const fs = require('fs');
 const crypto = require('crypto');
-class UsersRepository{
-    constructor(filename){
-        if(!filename){
+class UsersRepository {
+    constructor(filename) {
+        if (!filename) {
             throw new Error('Creating a repository requires a filename');
         }
 
         this.filename = filename;
-        try{
+        try {
             fs.accessSync(this.filename);
-        }catch(err){
+        } catch (err) {
             fs.writeFileSync(this.filename, '[]');
         }
     }
 
-    async getAll(){
-        return JSON.parse(await fs.promises.readFile(this.filename,{
+    async getAll() {
+        return JSON.parse(await fs.promises.readFile(this.filename, {
             encoding: 'utf-8'
         }));
     }
 
-    async create(attrs){
+    async create(attrs) {
         attrs.id = this.randomId();
         const records = await this.getAll();
         records.push(attrs);
         await this.writeAll(records);
+        return attrs;
     }
 
-    async writeAll(records){
+    async writeAll(records) {
         await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2));
     }
 
-    randomId(){
+    randomId() {
         return crypto.randomBytes(4).toString('hex');
     }
 
-    async getOne(id){
+    async getOne(id) {
         const records = await this.getAll();
         return records.find(record => record.id === id);
     }
 
-    async delete(id){
+    async delete(id) {
         const records = await this.getAll();
-        const filteredRecords =  records.filter(record => record.id !== id);
+        const filteredRecords = records.filter(record => record.id !== id);
         await this.writeAll(filteredRecords);
     }
 
-    async update(id, attrs){
+    async update(id, attrs) {
         const records = await this.getAll();
-        const record =  records.find(record => record.id === id);
+        const record = records.find(record => record.id === id);
 
-        if(!record){
+        if (!record) {
             throw new Error(`Record with id: ${id} not found`);
         }
 
@@ -58,19 +59,19 @@ class UsersRepository{
         await this.writeAll(records);
     }
 
-    async getOneBy(filters){
+    async getOneBy(filters) {
         const records = await this.getAll();
 
-        for(let record of records){
+        for (let record of records) {
             let found = true;
 
-            for(let key in filters){
-                if(record[key] !== filters[key]){
+            for (let key in filters) {
+                if (record[key] !== filters[key]) {
                     found = false;
                 }
             }
 
-            if(found){
+            if (found) {
                 return record;
             }
         }
