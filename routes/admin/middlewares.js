28bc939edbc40b6e92590674acc3f36d6552315a
cyberlearner13 <arkadeepmukherjee2012@gmail.com>
2021-05-07 23:59:24 +0530

    added edit product

--- routes/admin/middlewares.js
+++ routes/admin/middlewares.js
@@ -3,17 +3,29 @@ const {
 } = require('express-validator');
 
 module.exports = {
-    handleErrors(templateFn) {
+    handleErrors(templateFn, dataCb) {
         // all middlewares are functions, hence a function must be returned
-        return (req, res, next) => {
+        return async (req, res, next) => {
             const errors = validationResult(req);
-            console.log(errors);
+
             if (!errors.isEmpty()) {
+                // call the function here to pass data to template
+                let data = {};
+                if (dataCb) {
+                    data = await dataCb(req);
+                }
                 return res.send(templateFn({
-                    errors
+                    errors,
+                    ...data
                 }));
             }
             next();
         }
+    },
+    requireAuth(req, res, next) {
+        if (!req.session.userId) {
+            return res.redirect('/signin');
+        }
+        next();
     }
 }
--- routes/admin/products.js
+++ routes/admin/products.js
@@ -3,32 +3,35 @@ const multer = require('multer');
 
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
-const productsIndexTemplate = require('../../views/products');
+const productsIndexTemplate = require('../../views/admin/products');
+const productsEditTemplate = require('../../views/admin/products/edit');
 const {
     requireTitle,
     requirePrice
 } = require('./validators');
 const {
-    handleErrors
+    handleErrors,
+    requireAuth
 } = require('./middlewares');
+const products = require('../../repositories/products');
 
 const router = express.Router();
 const upload = multer({
     storage: multer.memoryStorage()
 });
 
-router.get('/admin/products', async (req, res) => {
+router.get('/admin/products', requireAuth, async (req, res) => {
     const products = await productsRepo.getAll();
     res.send(productsIndexTemplate({
         products
     }));
 });
 
-router.get('/admin/products/new', (req, res) => {
+router.get('/admin/products/new', requireAuth, (req, res) => {
     res.send(productsNewTemplate({}));
 });
 
-router.post('/admin/products/new', upload.single('image'), [requireTitle, requirePrice], handleErrors(productsNewTemplate), async (req, res) => {
+router.post('/admin/products/new', requireAuth, upload.single('image'), [requireTitle, requirePrice], handleErrors(productsNewTemplate), async (req, res) => {
 
     const image = req.file.buffer.toString('base64');
     const {
@@ -44,5 +47,36 @@ router.post('/admin/products/new', upload.single('image'), [requireTitle, requir
     res.redirect('/admin/products');
 });
 
+router.get('/admin/products/:id/edit', requireAuth, async (req, res) => {
+    const product = await productsRepo.getOne(req.params.id);
+
+    if (!product) return res.send('Product not found');
+
+    res.send(productsEditTemplate({
+        product
+    }));
+});
+
+router.post('/admin/products/:id/edit', requireAuth, upload.single('image'), [requireTitle, requirePrice], handleErrors(productsEditTemplate, async (req) => {
+    const product = await productsRepo.getOne(req.params.id);
+    return {
+        product
+    };
+}), async (req, res) => {
+    const changes = req.body;
+
+    if (req.file) {
+        changes.image = req.file.buffer.toString('base64');
+    }
+
+    try {
+        await productsRepo.update(req.params.id, changes);
+    } catch (err) {
+        return res.send('Could not find item');
+    }
+
+    res.redirect('/admin/products');
+
+});
 
 module.exports = router;
